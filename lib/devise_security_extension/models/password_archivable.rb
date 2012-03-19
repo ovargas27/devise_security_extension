a91588eec6165768f3c2e7cabf6b698d28a6ec5d
Omar Vargas <ovargas27@gmail.com>
2012-03-19 16:40:19 -0700

    Fix bug with old passwords

--- lib/devise_security_extension/models/password_archivable.rb
+++ lib/devise_security_extension/models/password_archivable.rb
@@ -6,7 +6,7 @@ module Devise
       extend  ActiveSupport::Concern
 
       included do
-        has_many :old_passwords, :as => :password_archivable, :dependent => :destroy, :order => 'id DESC'
+        has_many :old_passwords, :as => :password_archivable, :dependent => :destroy
         before_update :archive_password
         validate :validate_password_archive
       end
@@ -26,11 +26,12 @@ module Devise
         end
 
         if self.class.deny_old_passwords > 0 and not self.password.nil?
-          self.old_passwords.limit(self.class.deny_old_passwords).each do |old_password|
+          self.old_passwords.order('id DESC').limit(self.class.deny_old_passwords).each do |old_password|
             dummy                    = self.class.new
             dummy.encrypted_password = old_password.encrypted_password
             dummy.password_salt      = old_password.password_salt if dummy.respond_to?(:password_salt)
             return true if dummy.valid_password?(self.password)
+            end
           end
         end
 
@@ -44,11 +45,11 @@ module Devise
         if self.encrypted_password_changed?
           if self.class.password_archiving_count.to_i > 0
             if self.respond_to?(:password_salt_change) and not self.password_salt_change.nil?
-              self.old_passwords.create! :encrypted_password => self.encrypted_password_change.first, :password_salt => self.password_salt_change.first
+              self.old_passwords.create! :encrypted_password => self.encrypted_password_change.last, :password_salt => self.password_salt_change.last
             else
-              self.old_passwords.create! :encrypted_password => self.encrypted_password_change.first
+              self.old_passwords.create! :encrypted_password => self.encrypted_password_change.last
             end
-            self.old_passwords.offset(self.class.password_archiving_count).destroy_all
+            self.old_passwords.order('id DESC').offset(self.class.password_archiving_count).destroy_all
           else
             self.old_passwords.destroy_all
           end
